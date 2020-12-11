63d1f2f7d91350db86fe0e6aa88e47944be8e277
Ryan Pearce <ryan.pearce@key.me>
2020-12-11 21:05:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,7 @@ def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   person = {
     "name" => Ryan Pearce,
-    "height" => 77,
+    "height" => 77
   }
   puts person
 end
