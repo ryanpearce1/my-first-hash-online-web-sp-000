014770775ff4029659902aff8a9726649198b4a3
Ryan Pearce <ryan.pearce@key.me>
2020-12-11 21:11:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,9 +23,10 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 shipping_manifest["oil paintings"]
 end
+
+
 
 def adding
   shipping_manifest = {
